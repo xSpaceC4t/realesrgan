6a9cc0efaf3f7e7418d40601b5edfa1b5263c781
xSpaceC4t <mikolajkrawczyk7sm@gmail.com>
2025-12-20 00:25:01 +0100

    WIP

---
+++ rrdbnet_tiny.py
@@ -0,0 +1,25 @@
+from tinygrad import Tensor, nn
+
+
+class ResidualDenseBlock:
+    def __init__(self, num_feat=64, num_grow_ch=32):
+        self.conv1 = nn.Conv2d(num_feat, num_grow_ch, 3, 1, 1)
+        self.conv2 = nn.Conv2d(num_feat + num_grow_ch, num_grow_ch, 3, 1, 1)
+        self.conv3 = nn.Conv2d(num_feat + 2 * num_grow_ch, num_grow_ch, 3, 1, 1)
+        self.conv4 = nn.Conv2d(num_feat + 3 * num_grow_ch, num_grow_ch, 3, 1, 1)
+        self.conv5 = nn.Conv2d(num_feat + 4 * num_grow_ch, num_feat, 3, 1, 1)
+        self.lrelu = nn.LeakyReLU(negative_slope=0.2, inplace=True)
+
+    def __call__(self, x):
+        x1 = self.lrelu(self.conv1(x))
+        x2 = self.lrelu(self.conv2(torch.cat((x, x1), 1)))
+        x3 = self.lrelu(self.conv3(torch.cat((x, x1, x2), 1)))
+        x4 = self.lrelu(self.conv4(torch.cat((x, x1, x2, x3), 1)))
+        x5 = self.conv5(torch.cat((x, x1, x2, x3, x4), 1))
+        # Empirically, we use 0.2 to scale the residual for better performance
+        return x5 * 0.2 + x
+
+
+x = Tensor.ones(1, 3, 32, 32)
+model = ResidualDenseBlock()
+print(model(x).numpy())
