73bc280a933f1f6a67f7c234a12dd78d4d0a8a84
xSpaceC4t <mikolajkrawczyk7sm@gmail.com>
2025-12-21 14:52:22 +0100

    WIP

--- run_tests.py
+++ run_tests.py
@@ -9,10 +9,6 @@ from srvgg_tch import SRVGGNetCompact as SRVGGNetTch
 from srvgg_tiny import SRVGGNetCompact as SRVGGNetTiny
 
 
-os.environ['DEBUG'] = '2'
-os.environ['CPU'] = '1'
-
-
 def load_pth(path, model):
     loadnet = torch.load(path, map_location=torch.device('cpu'))
     if 'params_ema' in loadnet:
@@ -22,13 +18,25 @@ def load_pth(path, model):
     model.load_state_dict(loadnet[keyname], strict=True)
 
 
+def test_model(model_name, model_tch, model_tiny):
+    load_pth(f'models_pth/{model_name}.pth', model_tch)
+    load_state_dict(model_tiny, safe_load(f'models_st/{model_name}.safetensors'))
+
+    out_tch = model_tch(torch.ones(1, 3, 32, 32)).detach().numpy()
+    out_tiny = model_tiny(Tensor.ones(1, 3, 32, 32)).numpy()
+    
+    np.testing.assert_allclose(out_tch, out_tiny, atol=1e-5, rtol=1e-5)
+    print(f'{model_name} -> OK')
+
+
 model_tch = RRDBNetTch(num_in_ch=3, num_out_ch=3, num_feat=64, num_block=23, num_grow_ch=32)
-load_pth('models_pth/RealESRGAN_x4plus.pth', model_tch)
-
 model_tiny = RRDBNetTiny(num_in_ch=3, num_out_ch=3, num_feat=64, num_block=23, num_grow_ch=32)
-load_state_dict(model_tiny, safe_load('models_st/RealESRGAN_x4plus.safetensors'))
+test_model('RealESRGAN_x4plus', model_tch, model_tiny)
 
-out_tch = model_tch(torch.ones(1, 3, 32, 32)).detach().numpy()
-out_tiny = model_tiny(Tensor.ones(1, 3, 32, 32)).numpy()
+model_tch = SRVGGNetTch(num_in_ch=3, num_out_ch=3, num_feat=64, num_conv=16, upscale=4, act_type='prelu')
+model_tiny = SRVGGNetTiny(num_in_ch=3, num_out_ch=3, num_feat=64, num_conv=16, upscale=4, act_type='prelu')
+test_model('realesr-animevideov3', model_tch, model_tiny)
 
-np.testing.assert_allclose(out_tch, out_tiny, atol=1e-5, rtol=1e-5)
+model_tch = SRVGGNetTch(num_in_ch=3, num_out_ch=3, num_feat=64, num_conv=32, upscale=4, act_type='prelu')
+model_tiny = SRVGGNetTiny(num_in_ch=3, num_out_ch=3, num_feat=64, num_conv=32, upscale=4, act_type='prelu')
+test_model('realesr-general-x4v3', model_tch, model_tiny)
