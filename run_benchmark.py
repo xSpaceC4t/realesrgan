629757d73a5d2ec06d3989b43c759feac486a94d
xSpaceC4t <mikolajkrawczyk7sm@gmail.com>
2025-12-21 17:27:53 +0100

    WIP

--- run_benchmark.py
+++ run_benchmark.py
@@ -1,4 +1,5 @@
 import os
+import argparse
 import torch
 import numpy as np
 from tinygrad import Tensor, TinyJit
@@ -19,21 +20,36 @@ def load_pth(path, model):
     model.load_state_dict(loadnet[keyname], strict=True)
 
 
-model_tch = SRVGGNetTch(num_in_ch=3, num_out_ch=3, num_feat=64, num_conv=32, upscale=4, act_type='prelu')
-model_tiny = SRVGGNetTiny(num_in_ch=3, num_out_ch=3, num_feat=64, num_conv=32, upscale=4, act_type='prelu')
-# test_model('realesr-general-x4v3', model_tch, model_tiny)
+parser = argparse.ArgumentParser()
+parser.add_argument('model_name', type=str)
+parser.add_argument('--skip-torch', action='store_true')
+args = parser.parse_args()
+
+if args.model_name == 'RealESRGAN_x4plus':
+    model_tch = RRDBNetTch(num_in_ch=3, num_out_ch=3, num_feat=64, num_block=23, num_grow_ch=32)
+    model_tiny = RRDBNetTiny(num_in_ch=3, num_out_ch=3, num_feat=64, num_block=23, num_grow_ch=32)
+elif args.model_name == 'realesr-animevideov3':
+    model_tch = SRVGGNetTch(num_in_ch=3, num_out_ch=3, num_feat=64, num_conv=16, upscale=4, act_type='prelu')
+    model_tiny = SRVGGNetTiny(num_in_ch=3, num_out_ch=3, num_feat=64, num_conv=16, upscale=4, act_type='prelu')
+elif args.model_name == 'realesr-general-x4v3':
+    model_tch = SRVGGNetTch(num_in_ch=3, num_out_ch=3, num_feat=64, num_conv=32, upscale=4, act_type='prelu')
+    model_tiny = SRVGGNetTiny(num_in_ch=3, num_out_ch=3, num_feat=64, num_conv=32, upscale=4, act_type='prelu')
 
 tile_size = 128
 num_samples = 50
+in_shape = (1, 3, tile_size, tile_size)
 
-for _ in tqdm(range(num_samples), desc='Torch'):
-    x = torch.rand(1, 3, tile_size, tile_size)
-    out = model_tch.forward(x)
+print(f'Running {args.model_name}...')
+
+if not args.skip_torch:
+    for _ in tqdm(range(num_samples), desc='Torch'):
+        x = torch.rand(*in_shape)
+        out = model_tch.forward(x)
 
 @TinyJit
 def jit(x):
-    return model_tiny(x).realize()
+    return model_tiny(x).numpy()
 
 for _ in tqdm(range(num_samples), desc='Tinygrad'):
-    x = Tensor.rand(1, 3, tile_size, tile_size)
-    jit(x)
+    x = Tensor.rand(*in_shape)
+    out = jit(x)
